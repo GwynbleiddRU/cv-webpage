8243ee7c08a4c5b9f39ac15d74b665ac0b9046ec
George Nosachev <nosachev.gosha@gmail.com>
2025-04-03 05:23:15 +0500

    fix: changed router basename and homepage for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
+  base: '/cv-webpage',
   server: {
     host: "::",
     port: 8080,
+  },
+  build: {
+    outDir: './docs',
+    emptyOutDir: true,
   },
   plugins: [
     react()
